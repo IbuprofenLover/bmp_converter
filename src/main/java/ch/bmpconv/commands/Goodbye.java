5526b5ac9d6cb897d6a0dbaee17adab1c6d6c80a
onelaYuri <pierre@gellet.com>
2025-09-24 15:07:44 +0200

    Initial commit

---
+++ src/main/java/ch/bmpconv/commands/Goodbye.java
@@ -0,0 +1,23 @@
+package ch.bmpconv.commands;
+
+import ch.bmpconv.Main;
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "goodbye", description = "Print a 'Goodbye World!' type of message.")
+public class Goodbye implements Callable<Integer> {
+
+    @CommandLine.ParentCommand protected Main parent;
+
+    @CommandLine.Option(
+            names = {"-f", "--farewells"},
+            description = "The farewells to address the user.",
+            defaultValue = "Goodbye")
+    protected String farewells;
+
+    @Override
+    public Integer call() {
+        System.out.println(farewells + " " + parent.getName() + "!");
+        return 0;
+    }
+}
